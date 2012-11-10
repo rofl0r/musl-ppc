afa6795c3f9910b77eb7520976c9fd41eff123bf
rofl0r <retnyg@gmx.net>
2012-11-10 03:57:02 +0100

    some fixes for pthread_arch.h
    CANCEL_REG_IP is still wrong. the ppc has no dedicated program counter
    register.

--- arch/ppc/pthread_arch.h
+++ arch/ppc/pthread_arch.h
@@ -1,6 +1,13 @@
-typedef pthread_t (*__pthread_self_func_t)(void) __attribute__((const));
+static inline struct pthread *__pthread_self()
+{
+	register char* tp __asm__("r2");
+	return (pthread_t)(tp - 0x7000 - sizeof(struct pthread));
+}
+                        
+#define TLS_ABOVE_TP
+#define TP_ADJ(p) ((char *)(p) + sizeof(struct pthread) + 0x7000)
 
-#define __pthread_self ((__pthread_self_func_t)0xffff0fe0)
+// offset of the PC register in mcontext_t, divided by the system wordsize
+#define CANCEL_REG_IP 16
+//#define CANCEL_REG_IP 18                        
 
-#define CANCEL_REG_SP 16
-#define CANCEL_REG_IP 18
