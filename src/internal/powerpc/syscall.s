92c5eebbff3d052d3f82bd34c059304d00d514e4
rofl0r <retnyg@gmx.net>
2012-11-11 03:27:04 +0100

    syscall.s: fix indentation

--- src/internal/powerpc/syscall.s
+++ src/internal/powerpc/syscall.s
@@ -1,23 +1,23 @@
-        .global __syscall
-        .type   __syscall,@function
+	.global __syscall
+	.type   __syscall,@function
 __syscall:
-        mflr    0
-        stw     0, -4(1)              # Save the return address.
-        mr      0, 3                  # Save the system call number
-        mr      3, 4                  # Shift the arguments: arg1
-        mr      4, 5                  # arg2
-        mr      5, 6                  # arg3
-        mr      6, 7                  # arg4
-        mr      7, 8                  # arg5
-        mr      8, 9                  # arg6
-        sc
-        mfcr    0                      # Check for an error
-        rlwinm  4, 0, 0, 3, 3        # by checking for bit 28.
-        cmplwi  0, 4, 0               # It is an error if non-zero.
-        beq     0, 1f                  # Jump if not an error.
-        neg     3, 3                  # Negate the error number.
+	mflr    0
+	stw     0, -4(1)              # Save the return address.
+	mr      0, 3                  # Save the system call number
+	mr      3, 4                  # Shift the arguments: arg1
+	mr      4, 5                  # arg2
+	mr      5, 6                  # arg3
+	mr      6, 7                  # arg4
+	mr      7, 8                  # arg5
+	mr      8, 9                  # arg6
+	sc
+	mfcr    0                      # Check for an error
+	rlwinm  4, 0, 0, 3, 3        # by checking for bit 28.
+	cmplwi  0, 4, 0               # It is an error if non-zero.
+	beq     0, 1f                  # Jump if not an error.
+	neg     3, 3                  # Negate the error number.
 1:      lwz     0, -4(1)              # Restore the return address.
-        mtlr    0
+	mtlr    0
 	blr
-        .end    __syscall
-        .size   __syscall, .-__syscall
+	.end    __syscall
+	.size   __syscall, .-__syscall
