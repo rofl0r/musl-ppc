c4bf20453eacd4349f2d4db24552ea5e9f4fa82b
rofl0r <retnyg@gmx.net>
2012-11-12 21:35:39 +0100

    fix sigsetjmp - we need to store the link register for the sigmask call

--- src/signal/powerpc/sigsetjmp.s
+++ src/signal/powerpc/sigsetjmp.s
@@ -12,9 +12,12 @@ sigsetjmp:
 	#3) else: we must call pthread_sigmask(SIG_SETMASK, 0, (sigset_t *)buf->__ss);
 	# thus store r3 on the stack, to restore it later
 	stw 3, -4(1)
+	# store old link reg
+	mflr 0
+	stw 0, -8(1)
 	# increase stack frame by 16
 	subi 1, 1, 16
-	# put pointer to setjmp buf into r5 (3rd arg)
+	# put pointer to ss buf into r5 (3rd arg)
 	addi 5, 3, 260
 	# put "2" i.e. SIG_SETMASK in r3
 	li 3, 2
@@ -24,6 +27,9 @@ sigsetjmp:
 	addi 1, 1, 16
 	#restore r3
 	lwz 3, -4(1)
+	#restore link reg
+	lwz 0, -8(1)
+	mtlr 0
 1:
 	b setjmp
 	
